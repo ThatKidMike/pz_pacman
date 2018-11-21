dc6e854fefc12bdd25de186aa6a82caee9bffd88
ThatKidMike <bamkozlowski@gmail.com>
2018-11-21 21:53:33 +0100

    Fixed movement
    Pacman movement is smoothen and less collisious - it's bascially done.

--- Pacman_projekt/Assets/Scripts/PacmanMovement.cs
+++ Pacman_projekt/Assets/Scripts/PacmanMovement.cs
@@ -5,10 +5,10 @@ using System;
 
 public class PacmanMovement : MonoBehaviour {
 
-    public float velocity = 4.0f;
+    public float velocity = 3f;
     private Vector2 go;
     private Vector2 direction = Vector2.right;
-    Vector2 position;
+    //Vector2 position;
     public GameObject find;
     public PillsSpawn lookFor;
 
@@ -18,15 +18,16 @@ public class PacmanMovement : MonoBehaviour {
 
         find = GameObject.Find("PillsSpawn");
         lookFor = find.GetComponent<PillsSpawn>();
-        position = gameObject.transform.position;
+        //position = gameObject.transform.position;
 
     }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
 
         CheckInput();
-        Move();
+        //Move();
+        moveToNxtPoint(direction);
 
 	}
 
@@ -34,56 +35,66 @@ public class PacmanMovement : MonoBehaviour {
     // Checks for User's input and adjusts the movement direction and the facing direction
     void CheckInput() {
       
-        if(Input.GetKey(KeyCode.LeftArrow) && valid(new Vector2(-1, 0))) {
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && /*valid(new Vector2(-2, 0)) &&*/ canMove(Vector2.left)) {
 
             if(!direction.Equals(Vector2.left)) {
                 transform.rotation = Quaternion.identity;
                 transform.Rotate(0, 0 - 180, 0);
+                direction = Vector2.left;
+                moveToNxtPoint(direction);
             }
-            direction = Vector2.left;
+            Debug.Log("Left");
 
-        } else if(Input.GetKey(KeyCode.RightArrow) && valid(new Vector2(1, 0))) {
+        } else if(Input.GetKeyDown(KeyCode.RightArrow) && /*valid(new Vector2(2, 0)) &&*/ canMove(Vector2.right)) {
 
             if(!direction.Equals(Vector2.right)) {
                 transform.rotation = Quaternion.identity;
+                direction = Vector2.right;
+                moveToNxtPoint(direction);
             }
-            direction = Vector2.right;
+            Debug.Log("Right");
 
-        } else if(Input.GetKey(KeyCode.UpArrow) && valid(new Vector2(0, 1))) {
+        } else if(Input.GetKeyDown(KeyCode.UpArrow) && /*valid(new Vector2(0, 2)) &&*/ canMove(Vector2.up)) {
 
             if(!direction.Equals(Vector2.up)) {
                 transform.rotation = Quaternion.identity;
                 transform.Rotate(0, 0, 0 + 90);
+                direction = Vector2.up;
+                moveToNxtPoint(direction);
             }
-            direction = Vector2.up;
-        
-        } else if(Input.GetKey(KeyCode.DownArrow) && valid(new Vector2(0, -1))) {
+            Debug.Log("Up");
+
+        } else if(Input.GetKeyDown(KeyCode.DownArrow) && /*valid(new Vector2(0, -2)) &&*/ canMove(Vector2.down)) {
 
             if(!direction.Equals(Vector2.down)) {
                 transform.rotation = Quaternion.identity;
                 transform.Rotate(0, 0, 0 - 90);
+                direction = Vector2.down;
+                moveToNxtPoint(direction);
             }
-            direction = Vector2.down;
+            Debug.Log("Down");
 
         }
   
     }
 
+    //Is not used for now
     void Move() {
 
-        //Vector2 position = (transform.localPosition += (Vector3)(direction * velocity) * Time.deltaTime); 
+         //Vector2 position = (transform.localPosition += (Vector3)(direction * velocity) * Time.deltaTime); 
         if (lookFor.xy[(int)Math.Round(transform.position.x) + 30, (int)Math.Round(transform.position.y) + 30] == 1) {
-            // go = Vector2.MoveTowards(position, position+direction, velocity * Time.deltaTime);
-            // gameObject.transform.position = go;
-            // position = go;
-
-            transform.position = Vector2.Lerp(transform.position, direction, velocity * Time.deltaTime);
-            position = transform.position;
+            transform.localPosition += (Vector3)(direction * velocity) * Time.deltaTime;
         }
-        //transform.localPosition += (Vector3)(direction * velocity) * Time.deltaTime;
 
+    }
 
+    void moveToNxtPoint(Vector2 d) {
 
+        Vector2 currPosition = transform.localPosition;
+        // transform.localPosition = new Vector2((int)Math.Round(currPosition.x + d.x), (int)Math.Round(currPosition.y + d.y));
+        Vector2 targetPos = new Vector2((int)Math.Round(currPosition.x + d.x), (int)Math.Round(currPosition.y + d.y));
+        transform.localPosition = Vector2.MoveTowards(currPosition, targetPos, velocity * Time.deltaTime);
+        
     }
 
     bool valid(Vector2 dir) {
@@ -91,6 +102,18 @@ public class PacmanMovement : MonoBehaviour {
         Vector2 pos = transform.position;
         RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
         return (hit.collider == GetComponent<Collider2D>());
+    }
+
+    bool canMove(Vector2 d) {
+
+        Vector2 currPosition = transform.localPosition;
+
+        if(lookFor.xy[(int)Math.Round(currPosition.x + d.x) + 30, (int)Math.Round(currPosition.y + d.y) + 30] == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
 
