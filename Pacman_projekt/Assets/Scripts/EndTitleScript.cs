ee770b35adf907eaf7827aa4de4aae812229a104
ThatKidMike <bamkozlowski@gmail.com>
2018-11-30 21:19:16 +0100

    End title screen work in progress

---
+++ Pacman_projekt/Assets/Scripts/EndTitleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndTitleScript : MonoBehaviour {
+
+    public GameObject pac;
+    public GameObject run_ghost;
+
+    float velocity = 4.0f;
+    Vector2 target = new Vector2(-349.09f, -267.71f);
+    Vector2 direction = Vector2.right;
+
+    // Use this for initialization
+    void Start () {
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (pac.transform.localPosition != (Vector3)target && run_ghost.transform.localPosition != (Vector3)target) {
+
+            pac.transform.localPosition += (Vector3)(direction * velocity) * Time.deltaTime;
+            run_ghost.transform.localPosition += (Vector3)(direction * velocity) * Time.deltaTime;
+
+        }
+
+    }
+}
